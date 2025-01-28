a13ee0ebdb8e597cda4488ea47dfc3721679cef0
sokebat <sokebat01@gmail.com>
2025-01-28 18:05:16 +0545

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-red-500">App</div>
   )
 }
 
---
+++ src/components/organisms/nav-bar.tsx
@@ -0,0 +1,5 @@
+const NavBar = () => {
+  return <div className="bg-[#D9D9D966]">NavBar</div>;
+};
+
+export default NavBar;
