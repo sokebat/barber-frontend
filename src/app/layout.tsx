eb3d5ceb1a29d1f704ba16f4559dddee76e63475
virtualsujit <sokebat01@gmail.com>
2025-03-01 20:00:22 +0545

    change readme.md

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-[1400px] mx-auto px-4`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  px-4`}
       >
         <MainNav />
         <CartProvider>{children}</CartProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { TestimonialsSection } from "@/components/testimonials-section";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
-      <main className="flex-1 overflow-x-hidden">
+      <main className="flex-1 overflow-x-hidden ">
         <HeroSection />
         <ServicesSection />
         <BookingSection />
